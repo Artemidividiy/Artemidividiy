9dc4dd31450d198c7835c09fe53de72a5948bc12
uselessxiv <artemiy.kasyanik@gmail.com>
2022-04-12 00:40:02 +0300

    fixing issue with this shit

--- README.md
+++ README.md
@@ -20,13 +20,5 @@
 [![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](http://shields.io/)
 
 [![Ryo-ma's github trophy](https://github-profile-trophy.vercel.app/?username=Artemidividiy&row=1)](https://github.com/ryo-ma/github-profile-trophy)
-
-<!-- BLOG-POST-LIST:START -->
-- [How to prevent GitHub from suspending your cronjob based triggers](https://dev.to/gautamkrishnar/how-to-prevent-github-from-suspending-your-cronjob-based-triggers-knf)
-- [How I built one of the top 20 most used Github Actions](https://www.gautamkrishnar.com/how-i-built-one-of-the-top-20-most-used-github-actions/)
-- [Show your latest dev.to posts automatically on your GitHub profile readme](https://dev.to/gautamkrishnar/show-your-latest-dev-to-posts-automatically-in-your-github-profile-readme-3nk8)
-- [God Mode in browsers: document.designMode = &quot;on&quot;](https://dev.to/gautamkrishnar/god-mode-in-browsers-document-designmode-on-2pmo)
-- [Skipping the Chrome &quot;Your connection is not private&quot; warning](https://dev.to/gautamkrishnar/quickbits-1-skipping-the-chrome-your-connection-is-not-private-warning-4kp1)
-<!-- BLOG-POST-LIST:END -->
 <!-- codeforces starts--> 
 <!-- codeforces ends -->
--- fetcher.py
+++ fetcher.py
@@ -2,23 +2,162 @@ from asyncio import constants
 import requests
 import re
 import json
+from rich.console import Console
+from python_graphql_client import GraphqlClient
+
+import feedparser
+import httpx
+import json
+import pathlib
+import re
+import os
 
 def get_last_cf():
     try:
         f = open("./constants.json")
         r = requests.get(json.load(f)["codeforces_user_rating"])
-        return r.json()['result'][-1]['newRating']
+        return str(r.json()['result'][-1]['newRating'])
     finally:
         pass
 
-def replace_chunk(content, marker, chunk, inline=False):
+def fetch_writing():
+    pass
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+
+TOKEN = os.environ.get("SIMONW_TOKEN", "")
+
+
+def replace_chunk(content, marker, chunk):
     r = re.compile(
-        r'<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->'.format(marker, marker),
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
         re.DOTALL,
     )
-    if not inline:
-        chunk = '\n{}\n'.format(chunk)
-    chunk = '<!-- {} starts -->{}<!-- {} ends -->'.format(marker, chunk, marker)
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
     return r.sub(chunk, content)
 
-print(get_last_cf())
+
+def make_query(after_cursor=None):
+    return """
+query {
+  viewer {
+    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        name
+        releases(last:1) {
+          totalCount
+          nodes {
+            name
+            publishedAt
+            url
+          }
+        }
+      }
+    }
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+
+def fetch_releases(oauth_token):
+    repos = []
+    releases = []
+    repo_names = set()
+    has_next_page = True
+    after_cursor = None
+
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+        print()
+        print(json.dumps(data, indent=4))
+        print()
+        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
+                repos.append(repo)
+                repo_names.add(repo["name"])
+                releases.append(
+                    {
+                        "repo": repo["name"],
+                        "release": repo["releases"]["nodes"][0]["name"]
+                        .replace(repo["name"], "")
+                        .strip(),
+                        "published_at": repo["releases"]["nodes"][0][
+                            "publishedAt"
+                        ].split("T")[0],
+                        "url": repo["releases"]["nodes"][0]["url"],
+                    }
+                )
+        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
+            "hasNextPage"
+        ]
+        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
+    return releases
+
+
+def fetch_tils():
+    sql = "select title, url, created_utc from til order by created_utc desc limit 5"
+    return httpx.get(
+        "https://til.simonwillison.net/til.json",
+        params={"sql": sql, "_shape": "array",},
+    ).json()
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://simonwillison.net/atom/entries/")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    releases = fetch_releases(TOKEN)
+    releases.sort(key=lambda r: r["published_at"], reverse=True)
+    md = "\n".join(
+        [
+            "* [{repo} {release}]({url}) - {published_at}".format(**release)
+            for release in releases[:5]
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+
+    tils = fetch_tils()
+    tils_md = "\n".join(
+        [
+            "* [{title}]({url}) - {created_at}".format(
+                title=til["title"],
+                url=til["url"],
+                created_at=til["created_utc"].split("T")[0],
+            )
+            for til in tils
+        ]
+    )
+    rewritten = replace_chunk(rewritten, "tils", tils_md)
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+
+    readme.open("w").write(rewritten)
+if __name__ == "__main__":
+    print(get_last_cf())
+    replace_chunk(marker="codeforces", content=open("README.md"), chunk=get_last_cf(), inline=True)
